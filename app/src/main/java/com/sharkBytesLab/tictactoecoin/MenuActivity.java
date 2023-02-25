98051c9bc95e579a4f8c70e7693c056b3b794103
shashankmangal10@gmail.com <Sunil@1234>
2023-02-25 11:57:48 +0530

    Update Ready 4

--- app/src/main/java/com/sharkBytesLab/tictactoecoin/MenuActivity.java
+++ app/src/main/java/com/sharkBytesLab/tictactoecoin/MenuActivity.java
@@ -6,12 +6,22 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
-public class MenuActivity extends AppCompatActivity {
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.MaxAdListener;
+import com.applovin.mediation.MaxError;
+import com.applovin.mediation.ads.MaxInterstitialAd;
 
-    private CardView share, rate, privacy, terms, feedback;
+import java.util.concurrent.TimeUnit;
+
+public class MenuActivity extends AppCompatActivity implements MaxAdListener {
+
+    private CardView share, rate, privacy, terms, feedback, supportUs;
+    private MaxInterstitialAd interstitialAd;
+    private int retryAttempt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +35,14 @@ public class MenuActivity extends AppCompatActivity {
         privacy = findViewById(R.id.policy);
         terms = findViewById(R.id.terms);
         feedback = findViewById(R.id.feedback);
+        supportUs = findViewById(R.id.supportUs);
 
         String email = "shashankmangal10@gmail.com";
         String subject = "Feedback of Tic Tac Toe Coin Game.";
+
+        interstitialAd = new MaxInterstitialAd( "dca34227c5952b50", this );
+        interstitialAd.setListener( this );
+        interstitialAd.loadAd();
 
         share.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,6 +97,17 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+        supportUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if ( interstitialAd.isReady() )
+                {
+                    interstitialAd.showAd();
+                }
+            }
+        });
+
         privacy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -113,5 +139,51 @@ public class MenuActivity extends AppCompatActivity {
         startActivity(new Intent(MenuActivity.this, MainActivity.class));
         finish();
 
+    }
+    @Override
+    public void onAdLoaded(final MaxAd maxAd)
+    {
+        // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
+        // Reset retry attempt
+        retryAttempt = 0;
+    }
+
+    @Override
+    public void onAdLoadFailed(final String adUnitId, final MaxError error)
+    {
+        // Interstitial ad failed to load
+        // AppLovin recommends that you retry with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+
+        retryAttempt++;
+        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                interstitialAd.loadAd();
+            }
+        }, delayMillis );
+    }
+
+    @Override
+    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
+    {
+        // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
+        interstitialAd.loadAd();
+    }
+
+    @Override
+    public void onAdDisplayed(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdClicked(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdHidden(final MaxAd maxAd)
+    {
+        // Interstitial ad is hidden. Pre-load the next ad
+        interstitialAd.loadAd();
     }
 }
