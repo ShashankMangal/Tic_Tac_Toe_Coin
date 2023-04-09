f25fe62bbed84fa1979360b91a5a10906f1de4cd
shashankmangal10@gmail.com <Sunil@1234>
2023-04-09 21:08:34 +0530

    app version 7 updated

--- app/src/main/java/com/sharkBytesLab/tictactoecoin/MainActivity.java
+++ app/src/main/java/com/sharkBytesLab/tictactoecoin/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.gridlayout.widget.GridLayout;
-
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ColorDrawable;
@@ -21,22 +20,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.applovin.mediation.MaxAd;
-import com.applovin.mediation.MaxAdListener;
-import com.applovin.mediation.MaxError;
-import com.applovin.mediation.ads.MaxAdView;
-import com.applovin.mediation.ads.MaxInterstitialAd;
-import com.applovin.sdk.AppLovinSdk;
-import com.applovin.sdk.AppLovinSdkConfiguration;
-import com.sharkBytesLab.tictactoecoin.R.color;
-
-import java.util.concurrent.TimeUnit;
+import com.unity3d.ads.IUnityAdsInitializationListener;
+import com.unity3d.ads.IUnityAdsLoadListener;
+import com.unity3d.ads.IUnityAdsShowListener;
+import com.unity3d.ads.UnityAds;
+import com.unity3d.services.ads.operation.show.ShowOperation;
 
 import www.sanju.motiontoast.MotionToast;
 import www.sanju.motiontoast.MotionToastStyle;
 
-public class MainActivity extends AppCompatActivity implements MaxAdListener{
+public class MainActivity extends AppCompatActivity {
 
 
     private int activePlayer = 0;
@@ -46,10 +39,55 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener{
     private ConstraintLayout mainLayout;
     private Button resetBtn;
     private ImageView menu;
-    private MaxAdView adView;
-    private MaxInterstitialAd interstitialAd;
-    private int retryAttempt;
+    private String placementId = "Interstitial_Android";
+    private String unityGameId = "5238829";
+    private boolean testMode = false;
+    IUnityAdsInitializationListener myAdsListener = new IUnityAdsInitializationListener() {
+        @Override
+        public void onInitializationComplete() {
+            Toast.makeText(MainActivity.this, "Ads Initialised", Toast.LENGTH_SHORT).show();
+            UnityAds.load(placementId, loadListener);
+        }
 
+        @Override
+        public void onInitializationFailed(UnityAds.UnityAdsInitializationError error, String message) {
+
+        }
+    };
+
+    IUnityAdsLoadListener loadListener = new IUnityAdsLoadListener() {
+        @Override
+        public void onUnityAdsAdLoaded(String placementId) {
+            Toast.makeText(MainActivity.this, "Ads Loaded", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onUnityAdsFailedToLoad(String placementId, UnityAds.UnityAdsLoadError error, String message) {
+
+        }
+    };
+
+    IUnityAdsShowListener showListener = new IUnityAdsShowListener() {
+        @Override
+        public void onUnityAdsShowFailure(String placementId, UnityAds.UnityAdsShowError error, String message) {
+
+        }
+
+        @Override
+        public void onUnityAdsShowStart(String placementId) {
+
+        }
+
+        @Override
+        public void onUnityAdsShowClick(String placementId) {
+
+        }
+
+        @Override
+        public void onUnityAdsShowComplete(String placementId, UnityAds.UnityAdsShowCompletionState state) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,30 +96,16 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener{
 
         getSupportActionBar().hide();
 
+        UnityAds.initialize(getApplicationContext(), unityGameId, testMode, myAdsListener);
+
         mainLayout = findViewById(R.id.mainLayout);
         resetBtn = findViewById(R.id.resetBtn);
         menu = findViewById(R.id.menu);
-        adView = findViewById(R.id.applovinAd);
 
         AnimationDrawable animationDrawable = (AnimationDrawable) mainLayout.getBackground();
         animationDrawable.setEnterFadeDuration(1500);
         animationDrawable.setExitFadeDuration(2000);
         animationDrawable.start();
-
-        // Please make sure to set the mediation provider value to "max" to ensure proper functionality
-        AppLovinSdk.getInstance( this ).setMediationProvider( "max" );
-        AppLovinSdk.initializeSdk( this, new AppLovinSdk.SdkInitializationListener() {
-            @Override
-            public void onSdkInitialized(final AppLovinSdkConfiguration configuration)
-            {
-
-            }
-        } );
-
-        adView.loadAd();
-        interstitialAd = new MaxInterstitialAd( "0ddea73fe38117c5", this );
-        interstitialAd.setListener( this );
-        interstitialAd.loadAd();
 
         resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,9 +126,7 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener{
                 }
 
                 try {
-                    if (interstitialAd.isReady()) {
-                        interstitialAd.showAd();
-                    }
+                    UnityAds.show(MainActivity.this, placementId, showListener);
                 } catch (Exception e) {
                    showToast(e.getMessage().toString());
                    Log.v("Reset Error", e.getMessage().toString());
@@ -137,53 +159,6 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener{
                 MotionToast.LONG_DURATION,
                 ResourcesCompat.getFont(this, R.font.helvetica_regular));
 
-    }
-
-    @Override
-    public void onAdLoaded(final MaxAd maxAd)
-    {
-        // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
-        // Reset retry attempt
-        retryAttempt = 0;
-    }
-
-    @Override
-    public void onAdLoadFailed(final String adUnitId, final MaxError error)
-    {
-        // Interstitial ad failed to load
-        // AppLovin recommends that you retry with exponentially higher delays up to a maximum delay (in this case 64 seconds)
-
-        retryAttempt++;
-        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
-
-        new Handler().postDelayed(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                interstitialAd.loadAd();
-            }
-        }, delayMillis );
-    }
-
-    @Override
-    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
-    {
-        // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
-        interstitialAd.loadAd();
-    }
-
-    @Override
-    public void onAdDisplayed(final MaxAd maxAd) {}
-
-    @Override
-    public void onAdClicked(final MaxAd maxAd) {}
-
-    @Override
-    public void onAdHidden(final MaxAd maxAd)
-    {
-        // Interstitial ad is hidden. Pre-load the next ad
-        interstitialAd.loadAd();
     }
 
     public void tapped(View view)
--- app/src/main/java/com/sharkBytesLab/tictactoecoin/MenuActivity.java
+++ app/src/main/java/com/sharkBytesLab/tictactoecoin/MenuActivity.java
@@ -2,26 +2,16 @@ package com.sharkBytesLab.tictactoecoin;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
-import com.applovin.mediation.MaxAd;
-import com.applovin.mediation.MaxAdListener;
-import com.applovin.mediation.MaxError;
-import com.applovin.mediation.ads.MaxInterstitialAd;
-
-import java.util.concurrent.TimeUnit;
-
-public class MenuActivity extends AppCompatActivity implements MaxAdListener {
+public class MenuActivity extends AppCompatActivity {
 
     private CardView share, rate, privacy, terms, feedback, supportUs;
-    private MaxInterstitialAd interstitialAd;
-    private int retryAttempt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +30,7 @@ public class MenuActivity extends AppCompatActivity implements MaxAdListener {
         String email = "shashankmangal10@gmail.com";
         String subject = "Feedback of Tic Tac Toe Coin Game.";
 
-        interstitialAd = new MaxInterstitialAd( "dca34227c5952b50", this );
-        interstitialAd.setListener( this );
-        interstitialAd.loadAd();
+
 
         share.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,10 +89,10 @@ public class MenuActivity extends AppCompatActivity implements MaxAdListener {
             @Override
             public void onClick(View view) {
 
-                if ( interstitialAd.isReady() )
-                {
-                    interstitialAd.showAd();
-                }
+//                if ( interstitialAd.isReady() )
+//                {
+//                    interstitialAd.showAd();
+//                }
             }
         });
 
@@ -139,51 +127,5 @@ public class MenuActivity extends AppCompatActivity implements MaxAdListener {
         startActivity(new Intent(MenuActivity.this, MainActivity.class));
         finish();
 
-    }
-    @Override
-    public void onAdLoaded(final MaxAd maxAd)
-    {
-        // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
-        // Reset retry attempt
-        retryAttempt = 0;
-    }
-
-    @Override
-    public void onAdLoadFailed(final String adUnitId, final MaxError error)
-    {
-        // Interstitial ad failed to load
-        // AppLovin recommends that you retry with exponentially higher delays up to a maximum delay (in this case 64 seconds)
-
-        retryAttempt++;
-        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
-
-        new Handler().postDelayed(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                interstitialAd.loadAd();
-            }
-        }, delayMillis );
-    }
-
-    @Override
-    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
-    {
-        // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
-        interstitialAd.loadAd();
-    }
-
-    @Override
-    public void onAdDisplayed(final MaxAd maxAd) {}
-
-    @Override
-    public void onAdClicked(final MaxAd maxAd) {}
-
-    @Override
-    public void onAdHidden(final MaxAd maxAd)
-    {
-        // Interstitial ad is hidden. Pre-load the next ad
-        interstitialAd.loadAd();
     }
 }
